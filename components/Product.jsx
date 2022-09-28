f833d8393fab27db29ad8ef3de7dd8cd9c417a7c
r-bytes <101632683+r-bytes@users.noreply.github.com>
2022-09-28 18:51:22 +0200

    fixes

--- components/Product.jsx
+++ components/Product.jsx
@@ -9,7 +9,7 @@ const Product = ({ product: {image, name, slug, price } }) => {
             <div className="product-card">
                 <img
                     src={urlFor(image && image[0])}
-                    width={150}
+                    width={125}
                     height={250}
                     className="product-image"
                 />
